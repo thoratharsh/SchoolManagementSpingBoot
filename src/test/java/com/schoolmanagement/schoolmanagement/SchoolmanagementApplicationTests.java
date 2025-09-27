6305694e445588f22b664002d2722ad866a17e58
Harshavardhan Thorat <thoratharsh@gmail.com>
2025-09-27 14:40:22 +0530

    init commit

---
+++ src/test/java/com/schoolmanagement/schoolmanagement/SchoolmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schoolmanagement.schoolmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
